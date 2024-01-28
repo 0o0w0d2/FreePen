fa75d607e574b8153ff5ed3b08c9e2e1ebaa62d1
zion <0o0w0d2@gmail.com>
2024-01-28 18:59:16 +0900

    db를 global에서 참조할 수 있도록 변경

--- db.js
+++ db.js
@@ -3,14 +3,16 @@ require('dotenv').config();
 
 const url = process.env.DB_URI;
 
-exports.connectToMongoDB = async () => {
+const connectToMongoDB = async () => {
     try {
-        let db;
         const client = new MongoClient(url);
         await client.connect();
         db = client.db('forum');
         console.log('MongoDB connected');
+        return db;
     } catch (err) {
         console.log(err);
     }
 };
+
+module.exports = connectToMongoDB;
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
-const { connectToMongoDB } = require('./db');
+const connectToMongoDB = require('./db');
 require('dotenv').config();
 
 app.use(express.static(__dirname + '/public'));
 
 const port = process.env.PORT;
 
+let db;
+
 const startServer = async () => {
-    await connectToMongoDB();
+    db = await connectToMongoDB();
     app.listen(port, () => {
         console.log(`running on http://localhost:${port}`);
     });
@@ -18,4 +20,10 @@ startServer();
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/list', async (req, res) => {
+    const postList = await db.collection('post').find().toArray();
+    console.log(postList);
+    res.send();
 });
