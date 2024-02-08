e41a317fe049936b301aee20dcabb95e5403c85f
zion <0o0w0d2@gmail.com>
2024-02-08 20:17:07 +0900

    comment 삭제

--- router/postRouter.js
+++ router/postRouter.js
@@ -276,7 +276,7 @@ postRouter.get('/search', async (req, res, next) => {
 });
 
 // % ajax % POST ) comment
-postRouter.post('/comment/:postId', async (req, res, next) => {
+postRouter.post('/comment/:postId', isLogin, async (req, res, next) => {
     const postId = req.params.postId;
     const content = req.body.comment;
 
@@ -298,6 +298,32 @@ postRouter.post('/comment/:postId', async (req, res, next) => {
         });
 
         res.status(201).send({ message: '댓글 작성 완료' });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(err.message);
+    }
+});
+
+postRouter.delete('/comment/:commentId', isLogin, async (req, res, next) => {
+    const commentId = req.params.commentId;
+
+    try {
+        const user = req.user._id;
+        const comment = await db
+            .collection('comment')
+            .findOne({ _id: new ObjectId(commentId) });
+
+        if (!user.equals(comment.author)) {
+            const error = new Error('댓글을 작성할 권한이 없습니다.');
+            error.statusCode = 400;
+            throw error;
+        }
+
+        await db
+            .collection('comment')
+            .deleteOne({ _id: new ObjectId(commentId) });
+
+        res.status(204).send({ message: '삭제 완료' });
     } catch (err) {
         console.log(err);
         res.status(500).send(err.message);
